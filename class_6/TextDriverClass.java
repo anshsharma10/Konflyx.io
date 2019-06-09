13a03d4e1cbf81ff24a284672e4375c89dff9f17
Cinderguy23 <45177553+Cinderguy23@users.noreply.github.com>
2019-06-09 02:09:23 -0400

    ?

--- class_6/TextDriverClass.java
+++ class_6/TextDriverClass.java
@@ -36,16 +36,34 @@ public class TextDriverClass extends JFrame{
    */
   public TextDriverClass () {
     
+    setSize(1080,720);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    setPreferredSize(new Dimension(1080,720));
     
-    Gameplay game = new Gameplay(1);
-    game.setLayout(null);
-    game.setPreferredSize(new Dimension(1080,720));
-    add(game);
-    game.next();
+    JPanel panel = new JPanel();
+    panel.setLayout(null);
+    add(panel);  
     
+    /*
+     Text t = new Text (new Color (25,25,25),  40,40,  false,  false,  true,  50,  20, "TEST", panel);  
+     Text t2 = new Text (null,  20,40,  true,  false,  false,  20,  540, "/TEST ONE/TEST TWO", panel);  
+     Text t2 = new Text (null,  20,40,  true,  false,  false,  20,  540, "/TEST ONE/TEST TWO", panel); */ 
+    PracticeRoom pr = new PracticeRoom(panel);
+    
+    //panel = t2.draw();     
+    //panel = t.draw();
+    
+    panel = pr.getPanel();
     setVisible(true);
     this.pack();
+    
+    /*try {
+     Thread.sleep(3000);
+     panel = t.erase();
+     } catch (Exception e) {
+     System.out.println(e);
+     }
+     }*/
     
   }
   
